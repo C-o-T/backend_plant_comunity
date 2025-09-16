43ce7dffda847130a1a54fd007416b3c00b09dde
duswn7740 <duswn7740@hanmail.net>
2025-09-16 14:38:38 +0900

    회원가입/ã/아이디중복검사/사업자번호중복검사 구현

--- src/main/java/com/green/backend_plant_comunity/member/dto/MemberDTO.java
+++ src/main/java/com/green/backend_plant_comunity/member/dto/MemberDTO.java
@@ -1,4 +1,20 @@
 package com.green.backend_plant_comunity.member.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class MemberDTO {
+  private String memId;
+  private String memPw;
+  private String memName;
+  private String memAddr;
+  private String memDetailAddr;
+  private String memTell;
+  private String memEmail;
+  private String memGrade;
+  private LocalDateTime joinDate;
+  private String memBusinessNum;
+  private String memBusinessName;
 }
--- src/main/java/com/green/backend_plant_comunity/member/mapper/MemberMapper.java
+++ src/main/java/com/green/backend_plant_comunity/member/mapper/MemberMapper.java
@@ -1,4 +1,16 @@
 package com.green.backend_plant_comunity.member.mapper;
 
+import com.green.backend_plant_comunity.member.dto.MemberDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface MemberMapper {
+  //회원가입
+  public void joinMember(MemberDTO memberDTO);
+
+  //회원아이디 중복검사
+  public int checkId(String memId);
+
+  //사업자번호 중복검사
+  public int checkBusinessNum(String memBusinessNum);
 }
