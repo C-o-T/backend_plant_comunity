c28687d2556a71aefffc424c4c82c0e9b4b81e8b
park1541 <a059771@gmail.com>
2025-10-10 11:30:08 +0900

    아 덧글인데 like로 만들었다.

---
+++ src/main/java/com/green/backend_plant_comunity/comment/mapper/CommentMapper.java
@@ -0,0 +1,19 @@
+package com.green.backend_plant_comunity.comment.mapper;
+
+import com.green.backend_plant_comunity.comment.dto.CommentDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+
+   //댓글 등록
+   void insertComment(CommentDTO commentDTO);
+
+   //게시글의 댓글 조회 (일반 댓글만)
+   List<CommentDTO> getCommentsByBoardNum(int boardNum);
+
+   //특정 댓글의 대댓글 조회
+   List<CommentDTO> getRepliesByParentNum(int parentCommentNum);
+}
---
+++ src/main/java/com/green/backend_plant_comunity/comment/service/CommentService.java
@@ -0,0 +1,35 @@
+package com.green.backend_plant_comunity.comment.service;
+
+import com.green.backend_plant_comunity.comment.dto.CommentDTO;
+import com.green.backend_plant_comunity.comment.mapper.CommentMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+   private final CommentMapper commentMapper;
+
+   //댓글 등록
+   @Transactional(rollbackFor = Exception.class)
+   public void writeComment(CommentDTO commentDTO){
+      commentMapper.insertComment(commentDTO);
+   }
+
+   //게시글의 댓글 조회 (대댓글 포함)
+   public List<CommentDTO> getCommentsByBoardNum(int boardNum){
+      // 1. 일반 댓글 조회
+      List<CommentDTO> comments = commentMapper.getCommentsByBoardNum(boardNum);
+      
+      // 2. 각 댓글의 대댓글 조회
+      for(CommentDTO comment : comments) {
+         List<CommentDTO> replies = commentMapper.getRepliesByParentNum(comment.getCommentNum());
+         comment.setReplies(replies);  // 대댓글 목록 세팅
+      }
+      
+      return comments;
+   }
+}
